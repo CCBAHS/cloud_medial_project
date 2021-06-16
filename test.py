59274a7df72a375404698128ead7fa80462dd0c1
Preet Viradiya <69113578+pinakin7@users.noreply.github.com>
2021-06-17 00:08:02 +0530

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import pymongo
-
-
-client = pymongo.MongoClient("mongodb+srv://bths:BThSProject1.0@testcluster.f7oii.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-
-db = client.test
-
-db.users.insert_one({'username':'bths','password':'BThSProject1.0'})
