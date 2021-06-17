49b8e2b9f6943a7fc45dbc14fba23762697828e8
Preet Viradiya <69113578+pinakin7@users.noreply.github.com>
2021-06-17 22:41:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # mstorage = MongoStorage('user','tracker',host='mongodb+srv://testcluster.f7oii.mongodb.net/myFirstDatabase',username='bths',password='BThSProject1.0')
 sql_db = SQLAlchemy(app)
 migrate = Migrate(app,sql_db)
-sstorage = SQLStorage(db=sql_db,table_name='tracker_1')
+sstorage = SQLStorage(db=sql_db,table_name='tracker_users')
 
 t = TrackUsage(app,[sstorage])
 
